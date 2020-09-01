d6fd04060b17026934e8b64f5f21679075deb324
Ashish Aggarwal <ashishaggarwal@Ashishs-MacBook-Air.local>
2020-09-02 00:21:37 +0530

    upating code to add confusion matrix

--- ImplementationHMM.py
+++ ImplementationHMM.py
@@ -7,6 +7,8 @@ it will give complete tag sequence as null because probabilites are muliplying -
 3.
 '''
 from nltk.corpus import brown as training_data
+from tabulate import tabulate
+from sklearn import metrics
 import pandas as pd
 import nltk
 from math import log
@@ -25,7 +27,7 @@ def setupData(tagged_sents_training_data):
         # tag_bigrams = list(nltk.bigrams([tag for (word, tag) in training_data_tagged_words_set1]))
         for (pre_tag, post_tag) in tag_bigrams:
             tag_tag_count_dictionary[(pre_tag, post_tag)] += 1
-        tag_count_dict['^'] += 1
+        #tag_count_dict['^'] += 1
 
 def buildObservationTable(unique_tag_set, unique_words):
     for tag in unique_tag_set:
@@ -75,15 +77,16 @@ def ViterbiHMMFirstOrder(a, b ):
     return tag_output_sequence
 
 
-
+expected_predicted_dict=defaultdict(int);
+false_positive=defaultdict(int);
+false_negative=defaultdict(int);
+confusion_matrix=defaultdict(int);
 
 splitFile=100
 for test_iteration in range(5):
     tests=test_iteration*100
     testf=test_iteration*100+100
     log_base=2
-
-    #variable setup
     fileIds = training_data.fileids()
     training_fileIds_set1 = fileIds[:tests]+fileIds[testf:]
     test_fileIds_set1 = fileIds[tests:testf]
@@ -98,29 +101,59 @@ for test_iteration in range(5):
     observation_table=defaultdict(int);
     transition_table=defaultdict(int);
 
-
-    #extract useful information from corpus
     setupData(training_data_tagged_sents_set1)
     unique_tag_set_list=list(tag_count_dict.keys())
     unique_word_set_list=list(word_count_dict.keys())
     unique_tag_set_list.insert(0,'^')
-
-    #build observation table and transition table
     buildObservationTable(unique_tag_set_list, unique_word_set_list)
     buildTransitionTable(unique_tag_set_list)
 
 
-    #execute viterbi and calculate percentage of accuracy
     correct_tag_count=0
     total_tag_count=0
     for ind, test_sents in enumerate(test_data_sents_set1):
         #print(test_sents)
-        expected_tag_seq=[tag for (word,tag) in expected_data_sents_set1[ind]]
+        actual_tag_seq=[tag for (word,tag) in expected_data_sents_set1[ind]]
         #print(expected_tag_seq)
-        output_tag_seq=ViterbiHMMFirstOrder(test_sents,unique_tag_set_list)
+        predicted_tag_seq=ViterbiHMMFirstOrder(test_sents,unique_tag_set_list)
         #print(output_tag_seq)
-        for i in range(len(expected_tag_seq)):
-            if(expected_tag_seq[i]==output_tag_seq[i]):
+        for i in range(len(actual_tag_seq)):
+            if(actual_tag_seq[i]==predicted_tag_seq[i]):
                 correct_tag_count+=1
             total_tag_count+=1
+            expected_predicted_dict[(actual_tag_seq[i],predicted_tag_seq[i])]+=1
     print("accuracy in iteration%d: %d%c"%(test_iteration+1,(correct_tag_count*100.0/total_tag_count),37))
+
+
+
+#calculate confusion matrix
+unique_tag_set_list.remove('^')
+tag_set_length=len(unique_tag_set_list)
+confusion_matrix=[[0]*(tag_set_length+1) for _ in range(tag_set_length+1)]
+per_tag_accuracy_matrix=[[0]*(tag_set_length+1) for _ in range(2)]
+tag_index_dict=defaultdict(int)
+for index, tag in enumerate(unique_tag_set_list):
+    tag_index_dict[tag]=index
+#rows actual tag #columns are predicted tags
+confusion_matrix[0][0]='actual tag on rows'
+per_tag_accuracy_matrix[0][0]='Tag'
+per_tag_accuracy_matrix[1][0]='Accuracy'
+for i in range(1,tag_set_length+1):
+    confusion_matrix[i][0]=unique_tag_set_list[i-1]
+    confusion_matrix[0][i]=unique_tag_set_list[i-1]
+    per_tag_accuracy_matrix[0][i]=unique_tag_set_list[i-1]
+
+for (actual_tag, predicted_tag), value in expected_predicted_dict.items():
+    confusion_matrix[tag_index_dict[actual_tag]+1][tag_index_dict[predicted_tag]+1]+=value
+
+#per tag accuracy calculation
+
+for i in range(1,tag_set_length+1):
+    sum=0
+    for j in range(1,tag_set_length+1):
+        sum+=confusion_matrix[i][j]
+    per_tag_accuracy_matrix[1][i]=format(confusion_matrix[i][i]/sum*1.0,'.4f')
+
+print(tabulate(confusion_matrix, tablefmt="fancy_grid"))
+print(tabulate(per_tag_accuracy_matrix, tablefmt="fancy_grid"))
+
